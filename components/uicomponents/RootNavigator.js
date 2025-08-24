817863e5c3318b753c4242334eccd8786320f45d
Rohan2519Y <rohankumar17645@gmail.com>
2025-08-24 13:45:43 +0530

    installing dependencies

---
+++ components/uicomponents/RootNavigator.js
@@ -0,0 +1,16 @@
+import {
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem
+} from '@react-navigation/drawer'
+import { View } from 'react-native'
+
+const Drawer=createDrawerNavigator()
+export default function RootNavigator() {
+    return (<>
+        <View>
+
+        </View>
+    </>)
+}
