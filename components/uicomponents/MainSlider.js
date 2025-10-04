58b686e8050b46ebb94aeabbcc2b7ac62a309b97
Rohan2519Y <rohankumar17645@gmail.com>
2025-10-04 21:04:57 +0530

    completed main slider

--- components/uicomponents/MainSlider.js
+++ components/uicomponents/MainSlider.js
@@ -1,9 +1,34 @@
-import { View } from "react-native";
-import TextBox from './TextBox'
-export default function SearchBar() {
+import { Dimensions, Image, View } from "react-native";
+import { serverURL } from "../../services/FetchNodeServices";
+import Carousel from "react-native-reanimated-carousel";
+
+const { width, height } = Dimensions.get("window");
+
+export default function MainSlider({ data }) {
+    // Convert string -> array of filenames
+    const images = data?.files ? data.files.split(",") : [];
+
     return (
-        <View >
-            <TextBox w={.96} icon="search" msg="Search your Products..." />
+        <View style={{ width: width }}>
+            <Carousel
+                autoPlay
+                autoPlayInterval={2000}
+                data={images}
+                height={width / 2}
+                loop
+                pagingEnabled
+                snapEnabled
+                width={width}
+                scrollAnimationDuration={1000}
+                renderItem={({ item }) => (
+                    <View style={{ flex: 1  ,borderWidth:1,justifyContent:'center' }}>
+                        <Image
+                            source={{ uri: `${serverURL}/images/${item}` }}
+                            style={{ width: width, height: height/2, resizeMode: "contain" }}
+                        />
+                    </View>
+                )}
+            />
         </View>
-    )
+    );
 }
--- screens/Home.js
+++ screens/Home.js
@@ -3,21 +3,34 @@ import { serverURL, getData } from '../services/FetchNodeServices'
 import { useEffect, useState } from "react";
 import SliderComponent from '../components/uicomponents/SliderComponent'
 import SearchBar from '../components/uicomponents/SearchBar'
+import MainSlider from '../components/uicomponents/MainSlider'
 export default function Home(props) {
   const [category, setCategory] = useState([])
+  const [banners, setBanners] = useState([])
   const fetchCategoryList = async () => {
     var result = await getData('userinterface/display_all_category')
     setCategory(result.data)
-    console.log(result.data)
+  }
+  const fetchAllBanner = async () => {
+    var result = await getData('userinterface/fetch_all_banner')
+    setBanners(result.data[0])
+    console.log(result.data[0])
   }
 
-  useEffect(function () { fetchCategoryList() }, [])
+  useEffect(function () {
+    fetchCategoryList()
+    fetchAllBanner()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <View style={{alignItems:'center',marginTop:10}}>
+      <View style={{ alignItems: 'center', marginTop: 10 }}>
         <SearchBar />
       </View>
       <SliderComponent data={category} />
+      <View>
+        <MainSlider data={banners} />
+      </View>
     </View>)
 }
 
