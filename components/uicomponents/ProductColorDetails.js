e4f94cd5140938d6cc1c2ff64ec14858b559a2ac
Rohan2519Y <rohankumar17645@gmail.com>
2025-10-11 17:07:39 +0530

    Update ProductColorDetails.js

--- components/uicomponents/ProductColorDetails.js
+++ components/uicomponents/ProductColorDetails.js
@@ -5,6 +5,7 @@ import { postData } from "../../services/FetchNodeServices";
 export default function ProductColorDetails({ item }) {
     console.log('itreeeeam', item.productid)
     const [details, setDetails] = useState([])
+    const [select, setSelect] = useState(item.productdetailsid)
 
     const fetchProductDetails = async () => {
         var result = await postData('userinterface/fetch_product_details_by_productid', { productid: item.productid })
@@ -16,13 +17,15 @@ export default function ProductColorDetails({ item }) {
     }, [])
 
     return (<View style={{ width: '100%' }}>
-        <Text style={{ color: 'white' }}>Brand Color</Text>
-        {details.map((item) => (
-            <View>
-                <Text style={{ color: '#ffffff' }}>{item.color}</Text>
-            </View>
-        ))}
-        <TouchableOpacity>
-        </TouchableOpacity>
+        <Text style={{ color: 'white', marginLeft: 10, fontSize: 16, fontWeight: 'bold', marginVertical: 10 }}>Brand Color</Text>
+        <View style={{ flexWrap: 'wrap', flexDirection: 'row' }}>
+            {details.map((item) => (
+                <View>
+                    <TouchableOpacity activeOpacity={0.6} onPress={() => setSelect(item.productdetailsid)} style={{ paddingHorizontal: 25, paddingVertical: 10, borderRadius: 5, borderWidth: 1, borderColor: item.productdetailsid == select ? '#00e9bf' : 'grey', marginVertical: 10, marginLeft: 10, alignSelf: 'flex-start' }}>
+                        <Text style={{ color: '#ffffff', fontWeight: 'semibold', fontSize: 15 }}>{item.color.charAt(0).toUpperCase() + item.color.slice(1)}</Text>
+                    </TouchableOpacity>
+                </View>
+            ))}
+        </View>
     </View>)
 }
